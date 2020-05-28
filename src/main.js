c291d45e0c6894b7cedf5e87dd72d9d158dd85bb
Lilibeth-Gaete <lilibeth.gaete@gmail.com>
2020-05-28 18:35:08 -0400

    Incorporación registro con Firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // Variables
-const root = document.getElementById("root");
+const root = document.getElementById('root');
+const login = document.getElementById('login');
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,27 +17,79 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-
-// LOGIN
+// AREA DE LOGIN
 const loginPage = () => {
-  root.innerHTML = '';
-  root.innerHTML = `<div id="login">
-                    <input type="email" placeholder="Correo electronico" id="email" class="loginInput"/> 
-                    <input type="password" placeholder="Contrasena" id="pass" class="loginInput"/>
-                    <button id="loginBtn" class="button">Iniciar sesion</button>
+  login.innerHTML = '';
+  login.innerHTML = `<div id="loginPage">
+                    <h1>¡Bienvenido!</h1>
+                    <h2>¡Comparte tu receta con nosotros!</h2>
+                    <input type="email" placeholder="Correo electrónico" id="emailA" class="loginInput"/> 
+                    <input type="password" placeholder="Contraseña" id="passwordA" class="loginInput"/>
+                    <button id="loginBtn" class="button">Iniciar sesión</button>
+                    <p>¿No tienes cuenta? Regístrate</p> <a href='#userRegisterPage' id="newAccount">Aquí</a>
                     </div>`;
 };
 
 loginPage();
 
+// CLICK LOGIN
 document.getElementById('loginBtn').addEventListener('click', () => {
+  const emailA = document.getElementById('emailA').value;
+  const passwordA = document.getElementById('passwordA').value;
+
+  firebase.auth().signInWithEmailAndPassword(emailA, passwordA)
+    .catch(function(error){
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage)
+       });
+});
+
+//AREA DE LOGIN
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    const displayName = user.displayName;
+    const email = user.email;
+    const emailVerified = user.emailVerified;
+    const photoURL = user.photoURL;
+    const isAnonymous = user.isAnonymous;
+    const uid = user.uid;
+    const providerData = user.providerData;
+    //document.getElementById('login').innerHTML=`<div><p>Estas regístrado ${user.email}</p></div>`
+    console.log(user);
+  } else {
+    //document.getElementById('login').innerHTML=`<div><p>No estas regístrado</p></div>`
+  }
+}); 
+
+// REGISTRO
+
+const userRegisterPage = () => {
+  root.innerHTML = '';
+  root.innerHTML = `<div id="userRegisterPage">
+                    <h1>Registrate</h1>
+                    <input type="text" placeholder="Nombre" id="name" class="loginInput"/> 
+                    <input type="email" placeholder="Correo electrónico" id="email" class="loginInput"/> 
+                    <input type="password" placeholder="Contraseña" id="pass" class="loginInput"/>
+                    <button id="userRegisterBtn" class="button">Crear cuenta</button>
+                    </div>`;
+};
+
+
+document.getElementById('newAccount').addEventListener('click', () => {
+  userRegisterPage();
+});
+
+document.getElementById('userRegisterBtn').addEventListener('click', () => {
+  const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const pass = document.getElementById('pass').value;
 
-  firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
-    // Handle Errors here.
+  firebase.auth().createUserWithEmailAndPassword(name,email, pass).catch(function (error) {
     const errorCode = error.code;
     const errorMessage = error.message;
     alert(errorCode);
   });
 });
+
