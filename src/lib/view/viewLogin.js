9cf8dc8901546351b9445c5e1fe2338310450b47
ariela tolosa <ariela.tolosa.silva@gmail.com>
2020-05-29 18:54:06 -0400

    cambios2.0

--- src/lib/view/viewLogin.js
+++ src/lib/view/viewLogin.js
@@ -2,16 +2,16 @@
 document.getElementById('root').innerHTML = `
 <div id='viewLogin'>
 <div >
-  <img src='/src/img/LogoLadoPink.jpg' />
+  <img src='/src/img/LogoLadoPink.jpg' >
   </div>
   <div>
   <img  src='/src/img/welcome.png'>
   </div>
-    <div id="form-login" class="form-login">
+    <div id="formLogin" class="formLogin">
       <form  >
       <input type="email" id="correoelectronico" name="correoelectronico" placeholder="Ingresa E-mail"/>
       <input type="password" placeholder="Ingresa Contraseña" minlength="6">
-      <button class="btn login-btn" id="btn-iniciar-registrar" data-action ='login'>Iniciar Sesión</button>
+      <button class="btn login-btn" id="btnIniciar" >Iniciar Sesión</button>
       </form>
     </div>
     </div>`;
