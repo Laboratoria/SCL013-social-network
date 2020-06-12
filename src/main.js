1c12d976834019e40f0e2cdf66d647e192191216
fer <amontecinoscontreras@gmail.com>
2020-06-12 00:16:24 -0400

    firestore cambios html

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 // importamos funciones 
-import {myFunction} from "./lib/index.js";
-import {login} from "./lib/view/templateLogin.js";
-import {home} from "./lib/view/templateHome.js";
-import {cerrarSesion} from "./lib/viewController.js";
-import {iniciarSesion} from "./lib/viewController.js";
+import {
+    myFunction
+} from "./lib/index.js";
+import {
+    login
+} from "./lib/view/templateLogin.js";
+import {
+    home
+} from "./lib/view/templateHome.js";
+import {
+    cerrarSesion
+} from "./lib/viewController.js";
+import {
+    iniciarSesion
+} from "./lib/viewController.js";
 
 
 myFunction();
@@ -20,7 +30,17 @@ firebase.auth().onAuthStateChanged((user) => {
         iniciarSesion()
     }
 });
- 
 
-
-
+var db = firebase.firestore();
+db.collection("users").add({
+        first: "Ada",
+        last: "Lovelace",
+        born: 1900,
+        hijos: 2,
+    })
+    .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function (error) {
+        console.error("Error adding document: ", error);
+    });
