08f1613ca6fdf1dff5df9ae17d1323d87040fc46
fer <amontecinoscontreras@gmail.com>
2020-06-11 23:41:34 -0400

    aceptar cambios

--- src/lib/view/templateHome.js
+++ src/lib/view/templateHome.js
@@ -59,6 +59,12 @@ export const home = () => {
 <a id="btnRecomiendo"></a>
 </div>
 </div>
+icacion"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in </p>
+<div id="interacciones">
+<a id="btnCompartir"></a>
+<a id="btnRecomiendo"></a>
+</div>
+</div>
 
 <div class="contenedorSalida"> 
     <a id="btnHome" href='#/home'></a>
