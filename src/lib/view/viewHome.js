21abeba43ed7b526d97edef16b758870b8e74aa0
ariela tolosa <ariela.tolosa.silva@gmail.com>
2020-06-16 11:40:15 -0400

    intentando subir imagnes

--- src/lib/view/viewHome.js
+++ src/lib/view/viewHome.js
@@ -1,4 +1,5 @@
 import { logout } from '../index.js';
+import {subirImagen} from '../controladores/uploadimage.js';
 
 export const home = () => {
   window.location.hash = '/home';
@@ -9,6 +10,12 @@ export const home = () => {
     </header>
     <body>
     <div id="bodyHome">
+    <div class= 'profileBar'>
+    <button img id= "profile">
+    <button img id= "friends">
+    <button img id= "notification">
+    <button img id="btnOut">
+    </div>
     <div id="form-save">
     <form id="form-publication" class="padding" maxlength=50 required>
         <textarea placeholder="¿Que quieres compartir?" id="publication" class="textarea-post"></textarea>
@@ -16,7 +23,7 @@ export const home = () => {
             <div>
                 <label for="fileButton" id="image"><i class="fa fa-picture-o btn-picture"
                         aria-hidden="true"></i></label>
-                <input type="file" class="hide" name="file" value="upload" id="fileButton" />
+                <input type="file" class="hide" name="file" value="upload" id="imagen" />
             </div>
             <select id="typePublication" class="btn-select" name="select">
                 <option value="public" selected>Público</option>
@@ -25,19 +32,15 @@ export const home = () => {
             <input type="button" id="sharePost" class="btn-share" value="Compartir">
         </div>
     </form>
-   
-    <div id="post"></div>
-  </div>
+ </div>     
+ <div id="post"></div>
   <br>
-
-
   <section>
     <ul id="notes-list" class="ul-parent">
     </ul>
   </section>
     <div class="iconSend"> 
     </div>
-
    <!--<div id= "editDelete">
     <img src="icons/delete.png">
     <img src="icons/edit.png">
@@ -46,138 +49,105 @@ export const home = () => {
     <img src="./icons/like.png">
     <img src="icons/comment.png">
     </div>-->
-
      </div>
-     </body>
-    <footer class= 'footerOrder'>
-    <button img id= "profile">
-    <button img id= "friends">
-    <button img id= "notification">
-    <button img id="btnOut">
-
-    </footer>`;
-    buildListPost();
-
+     </body>`;
+  buildListPost();
   // BOTON QUE CREA CUENTA PARA NUEVO USUARIO
   document.getElementById('btnOut').addEventListener('click', () => {
     logout();
   });
-
     // BOTÓN PARA POSTEAR
     document.getElementById('sharePost').addEventListener('click', () => {
-
-    
-
       const user = firebase.auth().currentUser;
       const uid = user.uid;
       const username = user.displayName;
       const publication = document.getElementById('publication').value;
       const typePublication = document.getElementById('typePublication').value;
       document.getElementById('publication').value = '';
-
       const get = firebase.firestore().collection('posts').add({
         uid,
         date: new Date(),
-        publication:  publication,
+        publication: publication,
         typePublication: typePublication,
         author: username,
-
       });
       return get;
       buildListPost();
+
   });
-
 };
-
-
-
 const buildListPost = () => {
-
  const id = firebase.auth().currentUser.uid;
   firebase.firestore().collection('posts').orderBy('date', 'desc')
     .onSnapshot((querySnapshot) => {
       const arr = [];
-      
       querySnapshot.forEach((doc) => {
         removeElement("div-"+doc.id);
         //if (id === doc.data().uid || doc.data().public === true) {
           arr.push({ data: doc.data(), idpost: doc.id });
         //}
-
       });
-  
-
-   
       const posts = document.getElementById('post');
-      let cont=0;
       arr.forEach((post) => {
         console.log(post);
-        cont++;
-
-        let div = document.createElement('div');
-        div.id = 'div-'+post.idpost; 
-        let divID = 'div-'+post.idpost; 
-        posts.appendChild(div);
-
-       let pnom = document.createElement('p');
-       pnom.id = 'p'+post.idpost;
-       div.appendChild(pnom);
-      
-
-       let texto = document.createTextNode(post.data.author);
-       pnom.appendChild(texto);
-      
-       let tArea = document.createElement('TEXTAREA');
-       tArea.id = 'coment'+post.idpost;
-       div.appendChild(tArea);
-       const coment = document.getElementById('coment'+post.idpost);
-       coment.value = post.data.publication;
-
-       let btnDel = document.createElement('button');
-       let idbtn = post.idpost;
-       btnDel.id = idbtn;
-       let btnDelEle = document.getElementById(idbtn);
-       let textBtn = document.createTextNode('Eliminar');
-       btnDel.appendChild(textBtn);
-       div.appendChild(btnDel);
-
-       document.getElementById(idbtn).addEventListener('click', function(event) {
-        let id = event.target.id;
+        let divID = 'div-'+post.idpost;
+        let pnom = 'p'+post.idpost;
+        let tArea = 'pub'+post.idpost;
+        let btnDel = 'del'+post.idpost;
+        let btnEdit = 'edit'+post.idpost;
+        let divList = document.createElement('div'); 
+        divList.id = divID;
+        divList.classList = 'mystyle';
+        posts.appendChild(divList);
+        document.getElementById(divID).innerHTML = `
+        <p id= "${pnom}"></p>
+        <textarea id="${tArea}" class="textAreaContent">${post.data.publication} </textarea>
+        <button id="${btnDel}">Eliminar</button>
+        <button id="${btnEdit}">Editar</button>
+        `
+        
+        
+               /*
+       ` <div class="post-footer">
+       <button id="likes-${id}" class="buttons btn-likes">
+       </button>
+       <span class="post-counter" id="likes-count-${id}"></span>
+       <span class="post-date">${date} - ${hour}</span>
+       </div>`
+*/
+        document.getElementById(btnEdit).addEventListener('click', function(event) {
+          let idDoc = post.idpost;
+          let text = document.getElementById(tArea).value;
+          editTextPost(idDoc,text);
+        });
+      document.getElementById(btnDel).addEventListener('click', function(event) {
+        let id = post.idpost;
         deletePost(id);
        });
-
       });
+     
     });
 };
-
 // Cambia el contenido de un post
-const editTextPost = async (uid, text) => {
-  await firebase.firestore().collection('posts').doc(`${uid}`).update({
-    content: text,
+const editTextPost = (uid, text) => {
+  firebase.firestore().collection('posts').doc(`${uid}`).update({
+    publication: text,
   });
-
-  const gettingInfo = await firebase.firestore().collection('posts').doc(`${uid}`).get();
+  /*const gettingInfo = await firebase.firestore().collection('posts').doc(`${uid}`).get();
   const postTextContent = gettingInfo.data().content;
-  return postTextContent;
+  return postTextContent;*/
 };
-
-
 export const deletePost = (uid) => {
   firebase.firestore().collection('posts').doc(`${uid}`).delete()
     .then(() => {
       let divId = 'div-'+uid;
       removeElement(divId);
     }).catch((error) => {
-     
     });
 };
-
-
 function removeElement(elementId) {
-
   if (document.getElementById(elementId)) { 
     let element = document.getElementById(elementId);
     element.parentNode.removeChild(element);
   }
-
 }
